884c17d68539e58d30ccfdfb1b2d9d596a293073
theoffice <damir.ckelet@gmail.com>
2021-07-29 00:01:16 +0300

    filter, filter in local storage status options

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,6 +2,50 @@ import "../scss/styles.scss";
 import { getListOfCharacters } from "./api";
 import throttle from "./utils/throttle";
 
+const getList = async (limit, offset) => {
+  const list = []
+  try {
+    list.push(...await getListOfCharacters({
+      limit,
+      offset,
+    }));
+  } catch (e) {
+    console.log("error = ", e);
+  }
+  return list
+};
+
+const setFilter = (filterData) => {
+  localStorage.setItem('filter', JSON.stringify({...filterData}))
+}
+
+const checkFilter = () => {
+  const filter = localStorage.getItem('filter')
+
+  if (filter) {
+    let result = null
+    try {
+      result = JSON.parse(filter)
+    } catch (e) {
+      console.error(e)
+    }
+    return result
+  }
+  return null
+}
+
+const checkActiveState = () => {
+  const filter = checkFilter()
+
+  if (filter && filter.active) {
+    const selector = `season-${filter.value}`
+
+    const button = document.querySelector(`[data-action="${selector}"]`)
+
+    button.classList.add('button_selected')
+  }
+}
+
 const createCharacterItem = (data) => {
   const list = document.querySelector(".characters__list");
 
@@ -50,13 +94,73 @@ const createCharacterItem = (data) => {
   list.insertAdjacentHTML("beforeend", layout);
 };
 
-const init = () => {
+const optionChild = (value) => {
+  return `<option value="${value}">${value}</option>`
+}
+
+const init = async () => {
+  const main = document.querySelector(".characters__list");
+
+  let currentFilter = checkFilter()
   let list = [];
-  let cachedList = [];
   let limit = 10;
   let offset = 0;
 
-  const main = document.querySelector(".characters__list");
+  checkActiveState()
+
+  const getStatuses = () => {
+    const select = document.querySelector('[data-action=status]')
+
+    select.innerHTML = ''
+
+    const statusList = new Set()
+
+    list.forEach((item) => {
+      statusList.add(item.status)
+    })
+
+
+    Array.from(statusList).forEach((status) => {
+      select.insertAdjacentHTML('beforeend', optionChild(status))
+    })
+    console.log(select);
+
+    console.log('st = ', );
+  }
+
+  const getListAndRenderIt = async () => {
+    const response = await getList(limit, offset)
+    let filtredData = []
+
+    currentFilter = checkFilter()
+
+    if (currentFilter && currentFilter.active) {
+      if (currentFilter.isArray) {
+        filtredData = response.filter((item) => {
+          return item[currentFilter.field].find((id) =>  {
+            return currentFilter.value === id
+          })
+        })
+      } else {
+        filtredData = response.filter((item) => {
+          const regExp = new RegExp(currentFilter.value)
+
+          return regExp.exec(item[currentFilter.field])
+        })
+      }
+    } else {
+      filtredData = response
+    }
+    list.push(...filtredData)
+
+    filtredData.forEach((item) => {
+      createCharacterItem(item);
+    });
+
+    getStatuses()
+  }
+
+  await getListAndRenderIt()
 
   const scrollHandler = () => {
     if (
@@ -67,58 +171,50 @@ const init = () => {
 
       getListAfterScroll();
     }
-  };
+  }
 
-  const getList = async () => {
-    list = [];
-    cachedList = [];
-
-    try {
-      list = await getListOfCharacters({
-        limit,
-        offset,
-      });
-
-      if (Array.isArray(list) && !list.length) {
-        main.removeEventListener("scroll", scrollHandler);
-
-        return;
-      }
-
-      list.forEach((item) => {
-        createCharacterItem(item);
-      });
-    } catch (e) {
-      console.log("error = ", e);
-    }
-  };
-
-  const getListAfterScroll = throttle(getList, 1000);
+  const getListAfterScroll = throttle(getListAndRenderIt, 1000);
 
   main.addEventListener("scroll", scrollHandler, true);
 
-  getList();
-
-  const filterData = (season) => {
-    list = cachedList.filter((item) => item.appearance == season);
-
-    main.innerHTML = "";
-
-    list.forEach((item) => {
-      createCharacterItem(item);
-    });
-  };
-
   const seasonButtons = document.querySelectorAll('[data-action*="season"]');
 
-  console.log("seasonButtons = ", seasonButtons);
+  const setCurrentBtnActive = (currentButton, buttons) => {
+    if (currentButton.classList.contains('button_selected')) {
+      currentButton.classList.remove('button_selected')
+
+      return
+    }
+
+    buttons.forEach((button) => {
+      if (button.classList.contains('button_selected')) {
+        button.classList.remove('button_selected')
+      }
+    })
+
+    currentButton.classList.add('button_selected')
+  }
 
   seasonButtons.forEach((button) => {
     const season = button.getAttribute("data-action").split("-")[1];
 
-    button.addEventListener("click", () => {
-      console.log("click ", Number(season));
-      filterData(Number(season));
+    button.addEventListener("click", async () => {
+      if (button.classList.contains('button_selected')) {
+        setFilter(null)
+      } else {
+        setFilter({
+          field: 'appearance',
+          value: Number(season),
+          isArray: true,
+          active: true
+        })
+      }
+      setCurrentBtnActive(button, seasonButtons)
+
+      main.innerHTML = "";
+      offset = 0
+
+      await getListAndRenderIt()
     });
   });
 };
